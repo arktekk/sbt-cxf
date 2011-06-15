e99dd32f17b5b75f8a0e99829e602f36b9eedb7c
Trygve Laugstøl <trygvis@inamo.no>
2011-06-15 15:45:34 +0200

    o Making it easier to specify the cxf version.

--- project/build/CxfPluginProject.scala
+++ project/build/CxfPluginProject.scala
@@ -1,7 +1,9 @@
 import sbt._
 
-class CxfPluginProject(info: ProjectInfo) extends PluginProject(info) with IdeaProject {	
-	val wsdlto = "org.apache.cxf" % "cxf-tools-wsdlto-core" % "2.3.0"
-	val jaxb = "org.apache.cxf" % "cxf-tools-wsdlto-databinding-jaxb" % "2.3.0"
-	val frontend = "org.apache.cxf" % "cxf-tools-wsdlto-frontend-jaxws" % "2.3.0"
+class CxfPluginProject(info: ProjectInfo) extends PluginProject(info) with IdeaProject {
+  val cxfVersion = "2.3.0"
+
+  val wsdlto = "org.apache.cxf" % "cxf-tools-wsdlto-core" % cxfVersion
+  val jaxb = "org.apache.cxf" % "cxf-tools-wsdlto-databinding-jaxb" % cxfVersion
+  val frontend = "org.apache.cxf" % "cxf-tools-wsdlto-frontend-jaxws" % cxfVersion
 }
