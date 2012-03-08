9fa27eb2ea2aa335d47ba29545cd99ff1b8bf7ae
Trygve Laugstøl <trygvis@inamo.no>
2012-03-08 13:18:05 +0100

    o Conditionally compiling WSDLs.

--- src/main/scala/CxfPlugin.scala
+++ src/main/scala/CxfPlugin.scala
@@ -1,19 +1,22 @@
 package no.arktekk
 
 import sbt._
-import classpath.ClasspathUtilities
+import sbt.classpath.ClasspathUtilities
 import sbt.Keys._
 import java.io.File
 
 object CxfPlugin extends Plugin {
-  
+
   object cxf {
     val Config = config("cxf").hide
     val wsdl2java = TaskKey[Seq[File]]("wsdl2java", "Generates java files from wsdls")
     val wsdls = SettingKey[Seq[Wsdl]]("wsdls", "wsdls to generate java files from")
-    
-    case class Wsdl(file:File, args:Seq[String])
-    
+
+    case class Wsdl(file: File, args: Seq[String], key: Option[String] = None) {
+      def outputDirectory(basedir: File) =
+        new File(basedir, key.getOrElse(args.mkString.hashCode.abs).toString).getAbsoluteFile
+    }
+
     val settings = Seq(
       ivyConfigurations += Config,
       version in Config := "2.4.2",
@@ -24,21 +27,32 @@ object CxfPlugin extends Plugin {
       )),
       wsdls := Nil,
       managedClasspath in wsdl2java <<= (classpathTypes in wsdl2java, update) map { (ct, report) =>
-        Classpaths.managedJars(Config, ct, report)
+          Classpaths.managedJars(Config, ct, report)
       },
       sourceManaged in Config <<= sourceManaged(_ / "cxf"),
-      managedSourceDirectories in Compile <+= (sourceManaged in Config),
-      wsdl2java <<= (wsdls, sourceManaged in Config, managedClasspath in wsdl2java) map { (wsdls, output, cp) =>
+      managedSourceDirectories in Compile <++= (wsdls, sourceManaged in Config) { (wsdls, basedir) =>
+        wsdls map { _.outputDirectory(basedir) }
+      },
+      wsdl2java <<= (wsdls, sourceManaged in Config, managedClasspath in wsdl2java) map { (wsdls, basedir, cp) =>
         val classpath = cp.files
-        for(wsdl <- wsdls){
-          val args = Seq("-d", output.getAbsolutePath) ++ wsdl.args :+ wsdl.file.getAbsolutePath
-          callWsdl2java(args, classpath)
+        val x =
+        for (wsdl <- wsdls) yield {
+          val output = wsdl.outputDirectory(basedir)
+          if(wsdl.file.lastModified() > output.lastModified()) {
+            val args = Seq("-d", output.getAbsolutePath) ++ wsdl.args :+ wsdl.file.getAbsolutePath
+            IO.delete(output)
+            callWsdl2java(args, classpath)
+            (output ** "*.java").get
+          }
+          else {
+            Seq.empty
+          }
         }
-        (output ** "*.java").get
+        x.flatten
       },
       sourceGenerators in Compile <+= wsdl2java)
 
-    def callWsdl2java(args:Seq[String], classpath:Seq[File]){
+    def callWsdl2java(args: Seq[String], classpath: Seq[File]) {
       // TODO: Use the built-in logging mechanism from SBT when I figure out how that work - trygve
       println("WSDL: " + args)
       val classLoader = ClasspathUtilities.toLoader(classpath)
@@ -47,7 +61,7 @@ object CxfPlugin extends Plugin {
       val constructor = WSDLToJava.getConstructor(classOf[Array[String]])
       val run = WSDLToJava.getMethod("run", ToolContext)
       val oldContextClassLoader = Thread.currentThread.getContextClassLoader
-      try{
+      try {
         // to satisfy the jaxb reflection madness classLoader requirements
         Thread.currentThread.setContextClassLoader(classLoader)
         val instance = constructor.newInstance(args.toArray)
@@ -56,10 +70,10 @@ object CxfPlugin extends Plugin {
         case e =>
           // TODO: Figure out if there is a better way to signal errors to SBT.
           // Some of the CXF exceptions contain output that's proper to show to
-          // the user as it explains the error that ocurred.
+          // the user as it explains the error that occurred.
           e.printStackTrace
           throw e
-      } finally{
+      } finally {
         Thread.currentThread.setContextClassLoader(oldContextClassLoader)
       }
     }
